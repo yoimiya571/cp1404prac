79255461926a4b3b00feed505db1b238b65c5957
asus <yuhao.guo@my.jcu.edu.au>
2025-10-12 06:22:50 +0800

    Add list indexing, slicing, and element modification examples

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+# Initial list
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Answer the following expressions:
+# numbers[0]  -> 3
+# numbers[-1] -> 2
+# numbers[3]  -> 1
+# numbers[:-1] -> [3, 1, 4, 1, 5, 9]
+# numbers[3:4] -> [1]
+# 5 in numbers -> True
+# 7 in numbers -> False
+# "3" in numbers -> False
+# numbers + [6, 5, 3] -> [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Change the first element of numbers to "ten" (the string)
+numbers[0] = "ten"
+
+# Change the last element of numbers to 1
+numbers[-1] = 1
+
+# Print all the elements from numbers except the first two
+print(numbers[2:])
+
+# Print whether 9 is an element of numbers
+print(9 in numbers)
